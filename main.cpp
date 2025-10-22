f67f5cc69397a242c4370aa15e261cb816e74667
Evan Liu <evanyxliu@gmail.com>
2025-10-22 18:29:19 -0400

    fixed main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
-#include "project/reactor.h"
-#include "project/acceptor.h"
-#include "project/connection_manager.h"
-#include <thread>
-#include <iostream>
+#include "project/admin_server.h"
 
 int main() {
-    ConnectionManager conn_mgr;
-    Reactor reactor(conn_mgr);
-    Acceptor acceptor(5555, reactor, conn_mgr);
-
-    std::thread accept_thread([&acceptor]() {
-        acceptor.start_accepting();
-    });
-
-    reactor.run_loop();
-
-    accept_thread.join();
+    AdminServer admin(5555);
+    admin.start();
     return 0;
 }
